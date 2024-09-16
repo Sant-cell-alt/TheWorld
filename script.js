52c22aa6d42a719eea513fe6a427989e04fc9584
Sant-cell-alt <santiagoyosoypro@gmail.com>
2024-09-16 09:22:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Menú desplegable (Dropdown)
-document.querySelector('#dropdown').addEventListener('mouseover', function () {
+document.querySelector('#dropdown').addEventListener('mouseover', function (4) {
     this.querySelector('.dropdown-content').style.display = 'block';
 });
 
-document.querySelector('#dropdown').addEventListener('mouseleave', function () {
+document.querySelector('#dropdown').addEventListener('mouseleave', function (3) {
     this.querySelector('.dropdown-content').style.display = 'none';
 });
 
@@ -22,11 +22,11 @@ const modal = document.getElementById('modal');
 const abrirModal = document.getElementById('abrirModal');
 const cerrarModal = document.getElementById('cerrarModal');
 
-abrirModal.addEventListener('click', function () {
+abrirModal.addEventListener('click', function (2) {
     modal.style.display = 'block';
 });
 
-cerrarModal.addEventListener('click', function () {
+cerrarModal.addEventListener('click', function (1) {
     modal.style.display = 'none';
 });
 
